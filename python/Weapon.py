d5d6a0189a7bf0a6ca2049bd92f43ae4a0b4cb2e
Tom Johnston <tomo.johnston.slbs@gmail.com>
2018-03-08 01:19:19 +0000

    corrected mistake - bullet is not a weapon

--- python/Weapon.py
+++ python/Weapon.py
@@ -1,11 +1,11 @@
 import random
-from Bullet import Bullet
 from Bomb import Bomb
 from EMP import EMP
 from NailBomb import NailBomb
+from PeaShooter import PeaShooter
 class Weapon:
     def __init__(self):
-        self.type = random.choice([Bullet(), Bomb(), EMP(), NailBomb()])
+        self.type = random.choice([PeaShooter(), Bomb(), EMP(), NailBomb()])
 
     def getWeapon(self):
         return self.type
