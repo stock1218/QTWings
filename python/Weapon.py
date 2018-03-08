845e6667687b5819f6e16b9ef93759876024fb90
Tom Johnston <tomo.johnston.slbs@gmail.com>
2018-03-08 09:40:26 +0000

    added more functions to weapon class

--- python/Weapon.py
+++ python/Weapon.py
@@ -3,9 +3,21 @@ from Bomb import Bomb
 from EMP import EMP
 from NailBomb import NailBomb
 from PeaShooter import PeaShooter
+from Vector import Vector
 class Weapon:
-    def __init__(self):
+    def __init__(self, x, y):
         self.type = random.choice([PeaShooter(), Bomb(), EMP(), NailBomb()])
+        self.pos = Vector(random.randrange(10, x - 10), random.randrange(10, x - 10))
+        self.pickedUp = False
 
     def getWeapon(self):
         return self.type
+
+    def draw(self, canvas):
+         canvas.draw_circle((self.pos.x, self.pos.y), 9, 1, 'Blue', 'Blue')
+
+    def isPickedUp(self):
+        return self.pickedUp
+
+    def pickUp(self):
+        self.pickedUp = True
