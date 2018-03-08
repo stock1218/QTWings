b8ff6bcb73931aeebb3df8870dfc284759fb9ebe
stock1218 <johnsamuels1218@gmail.com>
2018-03-08 00:35:54 +0000

    Add implementation for PickUp.tick()

--- python/PickUp.py
+++ python/PickUp.py
@@ -3,17 +3,29 @@ try:
 except ImportError:
     import SimpleGUICS2Pygame.simpleguics2pygame as simplegui
 
+from Weapon import Weapon
+from PowerUp import PowerUp
 from random import randrange
 
 class PickUp:
 
     def tick(self):
         """Add a random type of pickup (PowerUp, Weapon(Gun, Bomb)) to pickups[] and reset the timer to a different interval"""
-        pass
+        type = random.choice(['PowerUp', 'Weapon'])	
+	
+        if(self.pickUps.length > self.maxPickUps):
+            self.pickUps.pop(0)
+
+        if(type == 'PowerUp'):
+            self.pickUps.append(PowerUp())
+        elif (type == 'Weapon'):
+            self.pickUps.append(Weapon())
+
 
     def __init__(self, min=9000, max=200000):
         """Construct the PickUp"""
         self.pickUps = []
+        self.maxPickUps = 5
         self.timer = simplegui.create_timer(randrange(min, max), self.tick)
         self.timer.start()
 
