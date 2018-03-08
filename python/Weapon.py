c632d096a83830fb32bb5ece2b26aa5b7d2cfdb3
Tom Johnston <tomo.johnston.slbs@gmail.com>
2018-03-08 01:00:33 +0000

    started work on weapons class

---
+++ python/Weapon.py
@@ -0,0 +1,8 @@
+import random
+from Bullet import Bullet
+from Bomb import Bomb
+from EMP import EMP
+from NailBomb import NailBomb
+class Weapon:
+    def __init__(self):
+        self.type = random.choice([Bullet(), Bomb(), EMP(), NailBomb()])
